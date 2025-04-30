584b10b2da81a1f4bc57575b94441e8f17f43c90
Inutilebot <orginu2025@outlook.it>
2025-04-30 11:09:18 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
 import socket
 import random
 import time
-import os
+import ssl  # Importa la libreria per SSL
 
 server = "irc.simosnap.com"
-port = int(os.getenv("PORT", 6667))  # Usa la porta di Render, altrimenti la porta di default (6667)
+port = 6697  # Cambia la porta a 6697 per supportare SSL
 nickname = "inutileorgasmo"
 password = "Inutili2025Orgasmi"
 channel = "#orgasmiinutili"
@@ -25,7 +25,13 @@ domande_surreali = [
     "Ti sei mai eccitato guardando un semaforo cambiare colore?",
 ] * 8
 
+# Crea la connessione socket
 irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Aggiungi supporto SSL alla connessione
+irc = ssl.wrap_socket(irc)
+
+# Connessione al server IRC con la porta 6697 (SSL)
 irc.connect((server, port))
 irc.send(f"PASS {password}\r\n".encode())
 irc.send(f"NICK {nickname}\r\n".encode())
@@ -33,16 +39,20 @@ irc.send(f"USER {nickname} 0 * :{nickname}\r\n".encode())
 irc.send(f"JOIN {channel}\r\n".encode())
 
 def send_message(msg):
+    """Funzione per inviare un messaggio nel canale IRC"""
     irc.send(f"PRIVMSG {channel} :{msg}\r\n".encode())
 
 while True:
     try:
+        # Ricevi i dati dal server IRC
         data = irc.recv(2048).decode(errors="ignore")
         print(data)
 
+        # Rispondi al PING con un PONG per mantenere la connessione attiva
         if data.startswith("PING"):
             irc.send(f"PONG {data.split()[1]}\r\n".encode())
 
+        # Rispondi ai comandi !domande e !stocazzo con le domande
         if "!domande" in data:
             domanda = random.choice(domande_normali)
             send_message(f"Domanda erotica: {domanda}")
@@ -51,6 +61,7 @@ while True:
             domanda = random.choice(domande_surreali)
             send_message(f"Domanda surreale: {domanda}")
 
+        # Aggiungi una piccola pausa per evitare sovraccarico
         time.sleep(1)
 
     except KeyboardInterrupt:
